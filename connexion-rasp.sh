f46da2cfc384a8be0b1e3ee6f7b7fe43787dd12c
Yakakliker <91472650+Yakakliker37@users.noreply.github.com>
2021-12-20 14:42:00 +0100

    Add files via upload

---
+++ connexion-rasp.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/expect
+
+set timeout 20
+
+set ip [lindex $argv 0]
+
+set user [lindex $argv 1]
+
+set password [lindex $argv 2]
+
+spawn ssh "$user\@$ip"
+
+expect "password:"
+
+send "$password\r";
+
+expect "administrateur@"
+
+send "https://raw.githubusercontent.com/Yakakliker37/Base_info/main/setup_raspberry.sh\r"
+
+expect "administrateur@"
+
+send "ls\r"
+
+expect "administrateur@"
+
+send "sudo chmod +x setup_raspberry.sh\r"
+
+expect "password"
+
+send "$password\r";
+
+expect "administrateur@"
+
+send "./setup_raspberry.sh\r"
+
+interact
+#expect eof
