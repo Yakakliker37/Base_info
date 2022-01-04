0a06c8f3a1a26824663bf05349a5520ee555c6a7
Yakakliker <91472650+Yakakliker37@users.noreply.github.com>
2022-01-04 16:46:05 +0100

    Add files via upload

--- connexion-aruba.sh
+++ connexion-aruba.sh
@@ -17,10 +17,19 @@ expect "6100#"
 send "config\r"
 expect "6100(config)#"
 
+send "ip dns server-address 8.8.8.8\r"
+expect "6100(config)#"
+
+send "ip dns server-address 8.8.4.4\r"
+expect "6100(config)#"
+
 send "vlan 2\r"
 expect "6100(config-vlan-2)#"
 
 send "description Isis\r"
+expect "6100(config-vlan-2)#"
+
+send "no shutdown\r"
 expect "6100(config-vlan-2)#"
 
 send "vlan 10\r"
@@ -29,10 +38,16 @@ expect "6100(config-vlan-10)#"
 send "description Video\r"
 expect "6100(config-vlan-10)#"
 
+send "no shutdown\r"
+expect "6100(config-vlan-10)#"
+
 send "vlan 11\r"
 expect "6100(config-vlan-11)#"
 
 send "description Intrusion\r"
+expect "6100(config-vlan-11)#"
+
+send "no shutdown\r"
 expect "6100(config-vlan-11)#"
 
 send "vlan 200\r"
@@ -41,10 +56,16 @@ expect "6100(config-vlan-200)#"
 send "description Wifi_Prive\r"
 expect "6100(config-vlan-200)#"
 
+send "no shutdown\r"
+expect "6100(config-vlan-200)#"
+
 send "vlan 300\r"
 expect "6100(config-vlan-300)#"
 
 send "description Wifi_Public\r"
+expect "6100(config-vlan-300)#"
+
+send "no shutdown\r"
 expect "6100(config-vlan-300)#"
 
 send "vlan 253\r"
@@ -53,10 +74,16 @@ expect "6100(config-vlan-253)#"
 send "description Vlan0253\r"
 expect "6100(config-vlan-253)#"
 
+send "no shutdown\r"
+expect "6100(config-vlan-253)#"
+
 send "vlan 255\r"
 expect "6100(config-vlan-255)#"
 
 send "description Vlan0255\r"
+expect "6100(config-vlan-255)#"
+
+send "no shutdown\r"
 expect "6100(config-vlan-255)#"
 
 send "interface 1/1/2-1/1/52\r"
@@ -66,6 +93,9 @@ send "vlan trunk native 1\r"
 expect "6100(config-if-<1/1/2-1/1/52>)#"
 
 send "vlan trunk allowed 1,2,10,11\r"
+expect "6100(config-if-<1/1/2-1/1/52>)#"
+
+send "no shutdown\r"
 expect "6100(config-if-<1/1/2-1/1/52>)#"
 
 send "exit\r"
