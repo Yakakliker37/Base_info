068cf694dfc6b529648d1ace220c5d2aa9d5e445
Yakakliker <91472650+Yakakliker37@users.noreply.github.com>
2023-03-12 16:20:26 +0100

    Add files via upload

---
+++ Zerotier.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+## Les variables
+: ${VAR01=whiptail}
+
+
+
+#####################################################
+fct001(){
+fct000 | sudo apt-get update -y
+
+sudo curl -s https://install.zerotier.com | sudo bash
+}
+
+
+######################################################
+fct002(){
+ID=$(whiptail --inputbox "Votre ID réseau ?" 8 39  --title "ID Réseau" 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+
+sudo zerotier-cli join $ID
+
+else
+    echo "Annulation."
+fi
+}
+
+
+########################################################
+fct003(){
+ID=$(whiptail --inputbox "Votre ID réseau ?" 8 39  --title "ID Réseau" 3>&1 1>&2 2>&3)
+
+
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+
+
+sudo zerotier-cli leave $ID
+
+else
+    echo "Annulation."
+fi
+}
+
+########################################################
+fct004(){
+
+sudo zerotier-cli listnetworks
+
+}
+########################################################
+fct005(){
+
+sudo zerotier-cli status
+
+}
+
+########################################################
+fct006(){
+
+sudo systemctl restart zerotier-one
+
+}
+
+
+############################################################
+if (whiptail --title "Zerotier" --yesno "Continuer ?" 8 78); then
+
+
+OPTION=$(whiptail --title "Zerotier" --menu "Que souhaitez vous faire ?" 15 60 6 \
+"fct001" "    Installer Zerotier" \
+"fct002" "    Vous connecter" \
+"fct003" "    Vous déconnecter" \
+"fct004" "    Lister les connexions réseau" \
+"fct005" "    Connaitre l'état du client Zerotier" \
+"fct006" "    Redémarrer le service Zerotier" 3>&1 1>&2 2>&3)
+ 
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+#    echo "Vous avez choisi la distribution : " $OPTION
+$OPTION
+
+else
+    echo "vous avez annulé"
+fi
+
+
+#########################################################
+
+## Fin du script
+
+else
+	echo "Fin."
+fi
+
+
