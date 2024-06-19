85bda31dd9e6741fc0a88af868cb28a350d64a07
Yakakliker <91472650+Yakakliker37@users.noreply.github.com>
2024-06-19 10:56:41 +0200

    Add files via upload

--- install-base.sh
+++ install-base.sh
@@ -20,6 +20,8 @@ export jaunegras="\033[33;1m"
 
 
 
+
+
 #####################################################
 fct001(){
 sudo apt-get update -y
@@ -29,9 +31,9 @@ sudo apt-get upgrade -y
 
 ######################################################
 fct002(){
-sudo apt-get install build-essential linux-headers-$(uname -r)
-sudo apt-get install net-tools
-sudo apt-get install rkhunter
+sudo apt-get install -q -y build-essential linux-headers-$(uname -r)
+sudo apt-get install -q -y net-tools
+sudo apt-get install -q -y rkhunter
 curl -O https://raw.githubusercontent.com/Yakakliker37/Base_info/main/Zerotier.sh
 
 
@@ -48,10 +50,10 @@ sudo curl -s https://install.zerotier.com | sudo bash
 ########################################################
 fct004(){
 
-sudo apt install ufw -y
+sudo apt-get install -q -y ufw -y
 sudo ufw allow ssh
 sudo ufw enable
-sudo apt install fail2ban -y
+sudo apt-get install -q -y fail2ban -y
 sudo systemctl enable fail2ban
 sudo systemctl start fail2ban
 
@@ -60,8 +62,109 @@ sudo systemctl start fail2ban
 ########################################################
 fct005(){
 
-sudo apt-get install postfix mailutils libsasl2-2 ca-certificates libsasl2-modules
+sudo apt-get install -q -y postfix mailutils libsasl2-2 ca-certificates libsasl2-modules
 sudo dpkg-reconfigure postfix
+cd ~
+sudo -S mv /etc/postfix/main.cf /etc/postfix/main.cf.inst
+sudo -S echo "
+# See /usr/share/postfix/main.cf.dist for a commented, more complete version
+
+
+# Debian specific:  Specifying a file name will cause the first
+# line of that file to be used as the name.  The Debian default
+# is /etc/mailname.
+#myorigin = /etc/mailname
+
+smtpd_banner = $myhostname ESMTP $mail_name (Ubuntu)
+biff = no
+
+# appending .domain is the MUA's job.
+append_dot_mydomain = no
+
+# Uncomment the next line to generate "delayed mail" warnings
+#delay_warning_time = 4h
+
+readme_directory = no
+
+# See http://www.postfix.org/COMPATIBILITY_README.html -- default to 3.6 on
+# fresh installs.
+compatibility_level = 3.6
+
+
+
+# TLS parameters
+smtpd_tls_cert_file=/etc/ssl/certs/ssl-cert-snakeoil.pem
+smtpd_tls_key_file=/etc/ssl/private/ssl-cert-snakeoil.key
+#smtpd_tls_security_level=may
+
+smtp_tls_CApath=/etc/ssl/certs
+
+smtp_tls_session_cache_database = btree:${data_directory}/smtp_scache
+
+smtp_tls_wrappermode = yes
+smtp_tls_security_level = encrypt
+
+
+smtpd_relay_restrictions = permit_mynetworks permit_sasl_authenticated defer_unauth_destination
+myhostname = monitor.yakakliker.org
+alias_maps = hash:/etc/aliases
+alias_database = hash:/etc/aliases
+#myorigin = /etc/mailname
+mydestination = monitor, localhost.localdomain, localhost
+#relayhost = 
+mynetworks = 127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128
+mailbox_size_limit = 0
+recipient_delimiter = +
+inet_interfaces = all
+inet_protocols = all
+
+relayhost = smtp-fr.securemail.pro:465
+
+smtp_sasl_auth_enable = yes
+smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd
+smtp_sasl_security_options =
+smtp_tls_CAfile = /etc/postfix/cacert.pem
+smtp_use_tls = yes
+
+smtp_sasl_mechanism_filter = login
+
+sender_canonical_maps = hash:/etc/postfix/sender_canonical
+recipient_canonical_maps = hash:/etc/postfix/recipient_canonical
+" >> main.cf
+sudo -S cp main.cf /etc/postfix/main.cf
+
+##########
+
+sudo -S echo "
+root										monitor@yakakliker.org
+root@monitor								monitor@yakakliker.org
+root@monitor.yakakliker.org					monitor@yakakliker.org
+administrateur			 					monitor@yakakliker.org
+administrateur@monitor 						monitor@yakakliker.org
+administrateur@monitor.yakakliker.org		monitor@yakakliker.org
+" >> sender_canonical
+sudo -S cp sender_canonical /etc/postfix/sender_canonical
+
+##########
+
+
+sudo -S echo "
+root										monitor@yakakliker.org
+root@monitor								monitor@yakakliker.org
+root@monitor.yakakliker.org					monitor@yakakliker.org
+administrateur			 					monitor@yakakliker.org
+administrateur@monitor 						monitor@yakakliker.org
+administrateur@monitor.yakakliker.org		monitor@yakakliker.org
+" >> recipient_canonical
+sudo -S cp recipient_canonical /etc/postfix/recipient_canonical
+
+##########
+#Création du certificat#
+
+cd ~
+cd /etc/ssl/certs
+sudo openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key-for-smtp-gmail.pem -out cert-for-smtp-gmail.pem -subj "/C=FR/ST=FRANCE/L=TOURS/O=YAKAKLIKER/OU=Web/CN=observium/emailAddress=webmaster@yakakliker.org"
+sudo -S cat /etc/ssl/certs/cert-for-smtp-gmail.pem | sudo tee -a /etc/postfix/cacert.pem
 
 }
 ############################################################
