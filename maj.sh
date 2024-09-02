03871f6f18abdcd652d7d3a53984a6fb700a6b2b
Yakakliker <91472650+Yakakliker37@users.noreply.github.com>
2024-09-02 11:40:21 +0200

    Add files via upload

---
+++ maj.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt update -y
+#export DEBIAN_FRONTEND=noninteractive
+#sudo -E apt-get -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" dist-upgrade -q -y --allow-downgrades --allow-remove-essential --allow-change-held-packages
