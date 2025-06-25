2d712366b8ee4948a197f2ade6ecc28e7faec47e
Yakakliker <91472650+Yakakliker37@users.noreply.github.com>
2025-06-25 11:26:39 +0200

    Update user-create.sh

--- user-create.sh
+++ user-create.sh
@@ -6,7 +6,7 @@ fct104
 
 
 ##########################################################
-	# Fonction Création d'un utilisateur
+# Fonction Création d'un utilisateur
 fct104() {
 	echo "$var25051201"																							# Affichage du nom de l'environnement
 	useradd -s /bin/bash -m "$var25051201"																					# Commande de création de l'environnement
