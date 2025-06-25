338d3ca55429bc77a21e6aca25e3b7ef2831f436
Yakakliker <91472650+Yakakliker37@users.noreply.github.com>
2025-06-25 11:25:35 +0200

    Update user-create.sh

--- user-create.sh
+++ user-create.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+cd / || exit
+var25051201=$(whiptail --inputbox "Entrez le nom de l'environnement" 10 60 3>&1 1>&2 2>&3)								# Initialisation de la variable d'environnement 
+
+
 
 ##########################################################
 	# Fonction Création d'un utilisateur
 fct104() {
-	echo "$var25051201"																										          # Affichage du nom de l'environnement
+	echo "$var25051201"																							# Affichage du nom de l'environnement
 	useradd -s /bin/bash -m "$var25051201"																					# Commande de création de l'environnement
-	passwd "$var25051201"																									          # Définition du mot de passe de l'environnement
+	passwd "$var25051201"																							# Définition du mot de passe de l'environnement
 }
 
