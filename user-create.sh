2bf37b3fcfbda1fceacf33602eb575240b3cbc3d
Yakakliker <91472650+Yakakliker37@users.noreply.github.com>
2025-06-25 11:23:46 +0200

    Create user-create.sh
    Fonction de création d'un utilisateur

---
+++ user-create.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+##########################################################
+	# Fonction Création d'un utilisateur
+fct104() {
+	echo "$var25051201"																										          # Affichage du nom de l'environnement
+	useradd -s /bin/bash -m "$var25051201"																					# Commande de création de l'environnement
+	passwd "$var25051201"																									          # Définition du mot de passe de l'environnement
+}
+
