bd4d63daf3950b7d73cb0bff746f789ffeaa8a25
Yakakliker <91472650+Yakakliker37@users.noreply.github.com>
2025-10-31 10:34:47 +0100

    Add files via upload

--- youtube.sh
+++ youtube.sh
@@ -8,32 +8,52 @@
 
 fct001(){
 
+url=$($VAR01 --title "URL" --inputbox "Entrez l'URL" 10 60 3>&1 1>&2 2>&3)
+
 yt-dlp -x --audio-format m4a $url
+
+fct999
+
+}
+
+fct002(){
+
+url=$($VAR01 --title "URL" --inputbox "Entrez l'URL" 10 60 3>&1 1>&2 2>&3)
+
+yt-dlp -x --audio-format m4a $url --no-playlist
+
+fct999
 
 }
 
 #####################################################
 
 
-############################################################
-if ($VAR01 --title "Download" --yesno "Souhaitez-vous télécharger une chanson ?" 10 60) then
+fct995() {
+############# Sélection de l'action à exécuter ############################
 
-url=$($VAR01 --title "URL" --inputbox "Entrez l'URL" 10 60 3>&1 1>&2 2>&3)
+		var25062504=$(NEWT_COLORS=${info[*]} whiptail --menu "$var25070203 : Que souhaitez vous faire ?" 25 60 15 \
+			"fct001" "    Télécharger une playlist" \
+			"fct002" "    Télécharger une chanson" \
+			3>&1 1>&2 2>&3)
 
-exitstatus=$?
+		exitstatus=$?
 		if [ $exitstatus = 0 ]; then
-
-
-fct001
-
-fi
-
-else
-    echo "vous avez annulé"
-fi
-
-
+			$var25062504																																			# Lancement de la fonction choisie
+		else
+			fct999
+		fi
+}
 #########################################################
 
-## Fin du script
 
+############# Fin du script ############################
+fct999() {
+	echo "(◕_◕) : That's all folks !"																																# Information de fin d'exécution du script
+}
+
+#### The Ultimate Software ! ####
+
+fct995
+
+## That's all
