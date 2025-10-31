9856c28c30aa9cac3e8e067ba46cf536ac3aa927
Yakakliker <91472650+Yakakliker37@users.noreply.github.com>
2025-10-31 14:45:33 +0100

    Add files via upload

--- youtube.sh
+++ youtube.sh
@@ -4,11 +4,45 @@
 : ${VAR01=whiptail}
 : ${url=}
 
+############ Les variables
+export selection="(◕_◕)"
+export var25042301="(°_°)"
+export var25070203="°(◕_◕)°"
+
+export var24051101
+var24051101=$(date +%y%m%d)																																			# Variable date année-mois-jour
+
+
+# Variables couleurs --- Pour faire sympa --- Oh la belle bleue !
+export red="\033[31m"																																				# Rouge
+export turquoise="\033[36m"																																			# Turquoise
+export gras="\033[1m"																																				# Gras
+export rougegras="\033[1;31m"																																		# Rouge Gras
+export reset="\033[0m"	
+
+export alert=(
+		"root=,red"
+		"window=,white"
+		"border=black,white"
+		"textbox=black,white"
+		"button=red,white"
+	)
+
+export info=(
+		"root=,blue"
+		"window=,white"
+		"border=black,white"
+		"textbox=black,white"
+		"button=red,white"
+	)
+
+	
+
 
 
 fct001(){
 
-url=$($VAR01 --title "URL" --inputbox "Entrez l'URL" 10 60 3>&1 1>&2 2>&3)
+url=$(NEWT_COLORS=${info[*]} whiptail --title "URL" --inputbox "Entrez l'URL" 10 60 3>&1 1>&2 2>&3)
 
 yt-dlp -x --audio-format m4a $url
 
@@ -18,7 +52,7 @@ fct999
 
 fct002(){
 
-url=$($VAR01 --title "URL" --inputbox "Entrez l'URL" 10 60 3>&1 1>&2 2>&3)
+url=$(NEWT_COLORS=${info[*]} whiptail --title "URL" --inputbox "Entrez l'URL" 10 60 3>&1 1>&2 2>&3)
 
 yt-dlp -x --audio-format m4a $url --no-playlist
 
