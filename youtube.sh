d536d5450bf1f4511ecda81b6a28126528251808
Yakakliker <91472650+Yakakliker37@users.noreply.github.com>
2026-01-08 13:26:25 +0100

    Add files via upload

--- youtube.sh
+++ youtube.sh
@@ -8,6 +8,7 @@
 export selection="(◕_◕)"
 export var25042301="(°_°)"
 export var25070203="°(◕_◕)°"
+export ver="PRY.260108"
 
 export var24051101
 var24051101=$(date +%y%m%d)																																			# Variable date année-mois-jour
@@ -20,21 +21,8 @@ export gras="\033[1m"																																				# Gras
 export rougegras="\033[1;31m"																																		# Rouge Gras
 export reset="\033[0m"	
 
-export alert=(
-		"root=,red"
-		"window=,white"
-		"border=black,white"
-		"textbox=black,white"
-		"button=red,white"
-	)
-
-export info=(
-		"root=,blue"
-		"window=,white"
-		"border=black,white"
-		"textbox=black,white"
-		"button=red,white"
-	)
+export alert="root=,red;window=,white;border=black,white;textbox=black,white;button=red,white"
+export info="root=,blue;window=,white;border=black,white;textbox=black,white;button=red,white"
 
 	
 
@@ -42,7 +30,7 @@ export info=(
 
 fct001(){
 
-url=$(NEWT_COLORS=${info[*]} whiptail --title "URL" --inputbox "Entrez l'URL" 10 60 3>&1 1>&2 2>&3)
+url=$(NEWT_COLORS="$info" whiptail --title "$ver" --inputbox "Entrez l'URL" 10 60 3>&1 1>&2 2>&3)
 
 yt-dlp -x --audio-format m4a $url
 
@@ -52,7 +40,7 @@ fct999
 
 fct002(){
 
-url=$(NEWT_COLORS=${info[*]} whiptail --title "URL" --inputbox "Entrez l'URL" 10 60 3>&1 1>&2 2>&3)
+url=$(NEWT_COLORS="$info" whiptail --title "$ver" --inputbox "Entrez l'URL" 10 60 3>&1 1>&2 2>&3)
 
 yt-dlp -x --audio-format m4a $url --no-playlist
 
@@ -66,7 +54,7 @@ fct999
 fct995() {
 ############# Sélection de l'action à exécuter ############################
 
-		var25062504=$(NEWT_COLORS=${info[*]} whiptail --menu "$var25070203 : Que souhaitez vous faire ?" 25 60 15 \
+		var25062504=$(NEWT_COLORS="$info" whiptail --menu "$var25070203 : Que souhaitez vous faire ?" --title "$ver" 25 60 15 \
 			"fct001" "    Télécharger une playlist" \
 			"fct002" "    Télécharger une chanson" \
 			3>&1 1>&2 2>&3)
